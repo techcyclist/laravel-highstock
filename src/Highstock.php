7b77fc00c729ff451fe9103b72f84701ff98ef04
Tim Norton <timothy.norton@gmail.com>
2019-03-20 08:54:52 -0600

    Update and rename Highcharts.php to Highstock.php

---
+++ src/Highstock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Techcyclist\Highstock;
+
+use Techcyclist\Highstock\Classes\Presenters\ChartPresenter;
+
+class Highstock extends ChartPresenter
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
