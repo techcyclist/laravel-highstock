2df594ab36b566522ed984022482c6510bf560c3
Tim Norton <timothy.norton@gmail.com>
2019-03-20 13:25:26 -0600

    Update JsTransformerPresenter.php

--- src/Classes/Presenters/JsTransformerPresenter.php
+++ src/Classes/Presenters/JsTransformerPresenter.php
@@ -4,82 +4,75 @@ namespace Techcyclist\Highstock\Classes\Presenters;
 
 class JsTransformerPresenter
 {
-    public $title = [];
+
+    public $accessibility = [];
+    public $annotations = [];
+    public $boost = [];
+    public $chart = [];
+    public $colors = [];
+    public $container = 'container';
+    public $credits = [];
+    public $data = [];
+    public $defs = [];
+    public $drilldown = [];
+    public $exporting = [];
+    public $labels = [];
+    public $legend = [];
+    public $loading = [];
+    public $navigation = [];
+    public $navigator = [];
+    public $nodata = [];
+    public $plotOptions = [];
+    public $rangeSelector = [];
+    public $responsive = [];
+    public $scrollbar = [];
+    public $series = [];
+    public $stockTools = [];
     public $subtitle = [];
+    public $time = [];
+    public $title = [];
+    public $tooltip = [];
     public $transform = [];
     public $yAxis = [];
     public $xAxis = [];
-    public $legend = [];
-    public $plotOptions = [];
-    public $series = [];
-    public $chart = [];
-    public $colors = [];
-    public $credits = [];
-    public $container = 'container';
-
-    public function __contruct()
+    
+    public function __construct()
     {
         $this->transform = '';
     }
 
-    public function encode_title()
+    public function credits()
     {
-        $data = $this->title;
+        $data = $this->credits;
 
-        $this->title = !empty($data) ? 'title: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+        $this->credits = !empty($data) ? 'credits: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
 
         return $this;
     }
 
-    public function encode_sub_title()
+    public function encode_accessibility()
     {
-        $data = $this->subtitle;
+        $data = $this->accessibility;
 
-        $this->subtitle = !empty($data) ? 'subtitle: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+        $this->accessibility = !empty($data) ? 'accessibility: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
 
         return $this;
     }
 
-    public function encode_y_axis()
+    public function encode_annotations()
     {
-        $data = $this->yAxis;
+        $data = $this->annotations;
 
-        $this->yAxis = !empty($data) ? 'yAxis: '.json_encode($data).',' : null;
+        $this->annotations = !empty($data) ? 'annotations: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
 
         return $this;
     }
 
-    public function encode_x_axis()
+    public function encode_boost()
     {
-        $data = $this->xAxis;
-        $this->xAxis = !empty($data) ? 'xAxis: '.json_encode($data).',' : null;
+        $data = $this->boost;
 
-        return $this;
-    }
-
-    public function encode_legend()
-    {
-        $data = $this->legend;
-
-        $this->legend = !empty($data) ? 'legend: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
-
-        return $this;
-    }
-
-    public function encode_plot_options()
-    {
-        $data = $this->plotOptions;
-
-        $this->plotOptions = !empty($data) ? 'plotOptions: '.json_encode($data).',' : null;
-
-        return $this;
-    }
-
-    public function encode_series()
-    {
-        $data = $this->series;
-
-        $this->series = !empty($data) ? 'series: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+        $this->boost = !empty($data) ? 'boost: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
 
         return $this;
     }
@@ -96,15 +89,206 @@ class JsTransformerPresenter
     public function encode_colors()
     {
         $data = $this->colors;
+
         $this->colors = !empty($data) ? 'colors: '.json_encode($data).',' : null;
 
         return $this;
     }
 
-    public function credits()
+    public function encode_data()
     {
-        $data = $this->credits;
-        $this->credits = !empty($data) ? 'credits: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+        $data = $this->data;
+
+        $this->data = !empty($data) ? 'data: '.json_encode($data).',' : null;
+
+        return $this;
+    }
+
+    public function encode_defs()
+    {
+        $data = $this->defs;
+
+        $this->defs = !empty($data) ? 'defs: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_drilldown()
+    {
+        $data = $this->drilldown;
+
+        $this->drilldown = !empty($data) ? 'drilldown: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_exporting()
+    {
+        $data = $this->exporting;
+
+        $this->exporting = !empty($data) ? 'exporting: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_labels()
+    {
+        $data = $this->labels;
+
+        $this->labels = !empty($data) ? 'labels: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_legend()
+    {
+        $data = $this->legend;
+
+        $this->legend = !empty($data) ? 'legend: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_loading()
+    {
+        $data = $this->loading;
+
+        $this->loading = !empty($data) ? 'loading: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_navigation()
+    {
+        $data = $this->navigation;
+
+        $this->navigation = !empty($data) ? 'navigation: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_navigator()
+    {
+        $data = $this->navigator;
+
+        $this->navigator = !empty($data) ? 'navigator: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_no_data()
+    {
+        $data = $this->noData;
+
+        $this->noData = !empty($data) ? 'noData: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_plot_options()
+    {
+        $data = $this->plotOptions;
+
+        $this->plotOptions = !empty($data) ? 'plotOptions: '.json_encode($data).',' : null;
+
+        return $this;
+    }
+
+    public function encode_range_selector()
+    {
+        $data = $this->rangeSelector;
+
+        $this->rangeSelector = !empty($data) ? 'rangeSelector: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_responsive()
+    {
+        $data = $this->responsive;
+
+        $this->responsive = !empty($data) ? 'responsive: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_scrollbar()
+    {
+        $data = $this->scrollbar;
+
+        $this->scrollbar = !empty($data) ? 'scrollbar: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_series()
+    {
+        $data = $this->series;
+
+        $this->series = !empty($data) ? 'series: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_stock_tools()
+    {
+        $data = $this->stockTools;
+
+        $this->stockTools = !empty($data) ? 'stockTools: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_sub_title()
+    {
+        $data = $this->subtitle;
+
+        $this->subtitle = !empty($data) ? 'subtitle: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_time()
+    {
+        $data = $this->time;
+
+        $this->time = !empty($data) ? 'time: '.json_encode($data).',' : null;
+
+        return $this;
+    }
+
+    public function encode_title()
+    {
+        $data = $this->title;
+
+        $this->title = !empty($data) ? 'title: '.json_encode($data, JSON_UNESCAPED_UNICODE).',' : null;
+
+        return $this;
+    }
+
+    public function encode_tooltip()
+    {
+        $data = $this->tooltip;
+
+        $this->tooltip = !empty($data) ? 'tooltip: '.json_encode($data).',' : null;
+
+        return $this;
+    }
+
+    public function encode_x_axis()
+    {
+        $data = $this->xAxis;
+
+        $this->xAxis = !empty($data) ? 'xAxis: '.json_encode($data).',' : null;
+
+        return $this;
+    }
+
+    public function encode_y_axis()
+    {
+        $data = $this->yAxis;
+
+        $this->yAxis = !empty($data) ? 'yAxis: '.json_encode($data).',' : null;
 
         return $this;
     }
