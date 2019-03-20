aa9b953d3d002a90a6e74db29f26ba3e0159d3cc
Tim Norton <timothy.norton@gmail.com>
2019-03-20 08:54:07 -0600

    Update Provider.php

--- src/Provider.php
+++ src/Provider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace RezaAr\Highcharts;
+namespace Techcyclist\Highstock;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -9,18 +9,18 @@ class Provider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/Config/highchart.php' => config_path('highchart.php'),
-        ], 'highchart_config');
+            __DIR__.'/Config/highstock.php' => config_path('highstock.php'),
+        ], 'highstock_config');
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/Config/highchart.php', 'highchart'
+            __DIR__.'/Config/highstock.php', 'highstock'
         );
 
-        $this->app->bind('register-highcharts', function () {
-            return new Highcharts();
+        $this->app->bind('register-highstock', function () {
+            return new Highstock();
         });
     }
 }
